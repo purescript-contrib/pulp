412c23eecc2da0ec79d17d799b54b2bf7a4d1b2d
Bodil Stokke <bodil@bodil.org>
2015-01-31 14:59:55 +0000

    Add in missing params for fs.write() for backwards compat.

--- shell.js
+++ shell.js
@@ -6,7 +6,7 @@ var log = require("./log");
 module.exports = function(cmd, callback) {
   log("Executing " + cmd);
   temp.open({"prefix": "pulp-cmd-", "suffix": ".sh"}, function(err, info) {
-    fs.write(info.fd, cmd, function(err) {
+    fs.write(info.fd, cmd, 0, encoding="utf-8", function(err) {
       if (err) {
         callback(err);
       } else {

839bb072e356947fe3338a8ecdfe2d47edb18e48
Isaac Shapira <fresheyeball@gmail.com>
2015-05-21 23:51:12 -0700

    gen

---
+++ gen.js
@@ -0,0 +1,64 @@
+var log = require("./log");
+var path = require("path");
+var fs = require("fs");
+
+function src(moduleName){
+  return "module " + moduleName + " where";
+}
+
+function test(moduleName){
+  return ["module Test." + moduleName + " where",
+  "",
+  "import " + moduleName,
+  "import Debug.Trace",
+  "",
+  "main = do",
+  "  trace \"Write tests for " + moduleName + "\""
+  ].join("\n") + "\n";
+}
+
+function write(file, trail, moduleName){
+  var modulePath = moduleName.split(".");
+
+  function prependBase(x){
+    return path.join.apply(this, [process.cwd()].concat(trail).concat([x]));
+  }
+
+  function writeFile(fileName){
+    var f = prependBase(fileName + ".purs");
+
+    if(fs.existsSync(f)){
+      console.log("file already exists : " + f );
+    }else{
+      console.log("writing file : " + f );
+      fs.writeFileSync(f, file(moduleName), "utf-8");
+    }
+  }
+
+  function writeDir(dirName){
+    var p = prependBase(dirName);
+    console.log("p",p);
+    if(!fs.existsSync(p)){
+      console.log(p);
+      fs.mkdirSync(p);
+      console.log("no way");
+      trail.push(dirName);
+    }
+  }
+
+  for(var i = 0; i < modulePath.length; i++){
+    var current = modulePath[i];
+    if(i === modulePath.length - 1){
+      writeFile(current);
+    }else{
+      writeDir(current);
+    }
+  }
+
+}
+
+module.exports = function(pro, args, callback) {
+  write(src, ["src"], args.remainder[0]);
+  write(test, ["test"], args.remainder[0]);
+  callback();
+}

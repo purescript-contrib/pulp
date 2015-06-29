5ca9c60e399cd1022e82aeb74e668eee46c7edb1
Phil Freeman <paf31@cantab.net>
2015-06-29 09:31:59 -0700

    Use psc/psc-bundle, not psc-make/psc

--- build.js
+++ build.js
@@ -5,31 +5,32 @@ var fs = require("fs");
 
 module.exports = function(pro, args, callback) {
   log("Building project in", process.cwd());
-  if (args.optimise) {
-    exec.psc(
-      [files.src, files.deps], 
-      [files.srcForeign, files.depsForeign], 
-      [
-        "--module=" + args.main, "--main=" + args.main
-      ].concat(args.remainder), null, function(err, src) {
-        if (err) return callback(err);
-        var out = args.to ? fs.createWriteStream(args.to) : process.stdout;
-        out.write(src, "utf-8", function(err) {
-          if (err) return callback(err);
-          log("Build successful.");
-          callback(null);
-        });
-      });
-  } else {
-    exec.pscMake(
-      [files.src, files.deps], 
-      [files.srcForeign, files.depsForeign], 
-      ["-o", args.buildPath].concat(args.remainder),
-      null, function(err, rv) {
-        if (err) return callback(err);
-        log("Build successful.");
+  
+  exec.psc(
+    [files.src, files.deps], 
+    [files.srcForeign, files.depsForeign], 
+    ["-o", args.buildPath].concat(args.remainder),
+    null, function(err, rv) {
+      if (err) return callback(err);
+      log("Build successful.");
+      if (args.optimise) {
+        log("Bundling Javascript...");
+        exec.pscBundle(
+          [files.outputModules(args.buildPath)], 
+          [
+            "--module=" + args.main, "--main=" + args.main
+          ].concat(args.remainder), null, function(err, src) {
+            if (err) return callback(err);
+            var out = args.to ? fs.createWriteStream(args.to) : process.stdout;
+            out.write(src, "utf-8", function(err) {
+              if (err) return callback(err);
+              log("Bundled.");
+              callback(null);
+            });
+          });
+      } else {
         callback(null);
       }
-    );
-  }
+    }
+  );
 };

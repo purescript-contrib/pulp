62714c890cc91cea1572e5f8682d9c1fb62b460e
Bodil Stokke <bodil@bodil.org>
2015-08-16 22:07:21 +0100

    Remove some commented code which isn't coming back.

--- browserify.js
+++ browserify.js
@@ -38,26 +38,6 @@ function optimising(pro, args, callback) {
     delete buildArgs.optimise;
     build(pro, buildArgs, fn);
   }
-  /* exec.psc(
-     globSet.sources(),
-     globSet.ffis(),
-     ["--module=" + args.main, "--main=" + args.main].concat(args.remainder),
-     null, function(err, src) {
-     if (err) return callback(err);
-     log("Compilation successful.");
-     log("Browserifying...");
-     var nodePath = process.env.NODE_PATH;
-     var buildPath = path.resolve(args.buildPath);
-     process.env["NODE_PATH"] = nodePath ? (buildPath + ":" + nodePath) : buildPath;
-     var b = browserify({
-     basedir: buildPath,
-     entries: new stringStream(src)
-     });
-     if (args.transform) b.transform(args.transform);
-     b.bundle().pipe(args.to ? fs.createWriteStream(args.to) : process.stdout)
-     .on("close", callback);
-     }
-     ); */
 }
 
 function incremental(pro, args, callback) {

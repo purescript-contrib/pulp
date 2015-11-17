b9f02d3c9d56623b1c6af317edbdb998c02a192b
Harry Garrood <harry@garrood.me>
2015-11-17 15:40:44 +0100

    Add missing FFI implementation for wait'

--- src/Pulp/System/ChildProcess.js
+++ src/Pulp/System/ChildProcess.js
@@ -5,3 +5,7 @@ exports["spawn'"] = function spawn$prime(cmd, args, env, stdio, callback) {
   var opts = { env: env, stdio: stdio };
   require("child_process").spawn(cmd, args, opts, callback);
 }
+
+exports["wait'"] = function wait$prime(child, callback) {
+  child.on("exit", callback);
+}

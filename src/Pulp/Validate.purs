951e8c7b061471e1b03dea7b9a7fc2197cd58d8a
Harry Garrood <harry@garrood.me>
2015-11-22 00:14:09 +0100

    Improve error when psc version can't be determined

--- src/Pulp/Validate.purs
+++ src/Pulp/Validate.purs
@@ -9,6 +9,7 @@ import Control.Monad (when)
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Exception (error)
+import Text.Parsing.Parser (ParseError(..))
 
 import Pulp.Exec (execQuiet)
 import Pulp.Data.Version
@@ -18,13 +19,23 @@ import qualified Pulp.System.Log as Log
 
 validate :: forall e. AffN e Unit
 validate = do
-  verStr <- trim <$> execQuiet "psc" ["--version"] Nothing
-  ver <- either (throwError <<< error <<< show) pure $ parseVersion verStr
+  ver <- getPscVersion
   when (ver < minimumPscVersion) $ do
     Log.err $ "This version of Pulp requires PureScript version 0.7.0.0 or higher."
-    Log.err $ "Your installed version is " <> verStr <> "."
+    Log.err $ "Your installed version is " <> showVersion ver <> "."
     Log.err $ "Please either upgrade PureScript or downgrade Pulp to version 3.x."
     liftEff $ exit 1
+
+getPscVersion :: forall e. AffN e Version
+getPscVersion = do
+  verStr <- trim <$> execQuiet "psc" ["--version"] Nothing
+  case parseVersion verStr of
+    Right v ->
+      pure v
+    Left (ParseError { message: msg }) -> do
+      Log.err $ "Unable to parse the version from psc. (It was: " <> verStr <> ")"
+      Log.err $ "Please check that the right psc is on your PATH."
+      liftEff $ exit 1
 
 minimumPscVersion :: Version
 minimumPscVersion = Version (toList [0, 7, 0, 0])

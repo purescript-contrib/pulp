53a3092874748a5d9d8211afea812d6a81a4aea8
Harry Garrood <harry@garrood.me>
2015-11-24 22:36:17 +0100

    Add Pulp.Args.Types.{file, directory}

--- src/Pulp/Args/Types.purs
+++ src/Pulp/Args/Types.purs
@@ -2,6 +2,8 @@ module Pulp.Args.Types
   ( flag
   , string
   , file
+  , int
+  , directory
   ) where
 
 import Prelude
@@ -13,6 +15,8 @@ import Control.Monad.Trans
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.List (List(..))
+import Data.Int (fromString)
+import Data.Foldable (elem)
 
 import Text.Parsing.Parser (ParserT(..))
 import Node.FS (FS())
@@ -21,29 +25,47 @@ import Node.FS.Aff (exists)
 import Pulp.Args
 import Pulp.Args.Parser
 
+argErr :: forall a. String -> String -> OptParser a
+argErr arg msg =
+  halt ("Argument " ++ arg ++ ": " ++ msg)
+
 flag :: OptionParser
 flag = {
   name: Nothing,
   parser: \_ -> return Nothing
   }
 
-stringParser :: forall e.
-  String -> ParserT (List String) (Aff (avar :: AVAR, node :: Node | e)) String
-stringParser arg =
-  token <|> halt ("Argument " ++ arg ++ ": Needs a string argument.")
-
 string :: OptionParser
 string = {
   name: Just "<string>",
-  parser: \arg -> Just <$> stringParser arg
+  parser: \arg -> Just <$> (token <|> argErr arg "Needs a string argument.")
+  }
+
+int :: OptionParser
+int = {
+  name: Just "<int>",
+  parser: \arg -> do
+    let err = argErr arg "Needs an int argument." :: forall a. OptParser a
+    mint <- fromString <$> (token <|> err)
+    case mint of
+      Just i -> return (Just (show i))
+      Nothing -> err
   }
 
 file :: OptionParser
 file = {
   name: Just "<file>",
   parser: \arg -> do
-    path <- stringParser arg
+    path <- token <|> argErr arg "Needs a file argument."
     e <- lift $ exists path
     if e then return $ Just path
       else halt ("File '" ++ path ++ "' not found.")
+  }
+
+directory :: OptionParser
+directory = {
+  name: Just "<directory>",
+  parser: \arg -> do
+    path <- token <|> argErr arg "Needs a directory argument."
+    return $ Just "lol"
   }

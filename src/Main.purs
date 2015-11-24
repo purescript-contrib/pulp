51194ddbd22f3da94cb13a6af661bcc8150f9d05
Harry Garrood <harry@garrood.me>
2015-11-25 00:33:39 +0100

    Bring Main up to date with index.js

--- src/Main.purs
+++ src/Main.purs
@@ -2,14 +2,22 @@ module Main where
 
 import Prelude
 
+import Control.Alt ((<|>))
+import Control.Monad.Error.Class (throwError)
 import Control.Monad.Aff
 import Control.Monad.Aff.AVar
 import Control.Monad.Eff
 import Control.Monad.Eff.Console (log, CONSOLE())
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Control.Monad.Eff.Exception
-import Data.Either (Either(..))
+import Data.Either (Either(..), either)
+import Data.Foreign (parseJSON)
+import Data.Foreign.Class (readProp)
 import Text.Parsing.Parser (ParseError(..))
 import Node.FS (FS())
+import Node.Encoding (Encoding(UTF8))
+import Node.FS.Sync (readTextFile)
+import qualified Node.Path as Path
 
 import qualified Pulp.Args as Args
 import Pulp.Args.Help
@@ -32,24 +40,49 @@ globals = [
     "Run a shell command after the operation finishes. Useful with `--watch`."
   ]
 
+defaultDependencyPath :: String
+defaultDependencyPath =
+  unsafePerformEff (catchException (const (pure "bower_components")) readFromBowerRc)
+  where
+  readFromBowerRc = do
+    json <- readTextFile UTF8 ".bowerrc"
+    case parseJSON json >>= readProp "directory" of
+      Right dir -> pure dir
+      Left err  -> throwException (error (show err))
+
+-- | Options for any command requiring paths
+pathArgs :: Array Args.Option
+pathArgs = [
+  Args.option "includePaths" ["--include", "-I"] Type.string
+    ("Additional directories for PureScript source files, separated by `" ++ Path.delimiter ++ "`."),
+  Args.optionDefault "srcPath" ["--src-path"] Type.directory
+    "Directory for PureScript source files." "src",
+  Args.optionDefault "testPath" ["--test-path"] Type.directory
+    "Directory for PureScript test files." "test",
+  Args.optionDefault "dependencyPath" ["--dependency-path"] Type.directory
+    "Directory for PureScript dependency files." defaultDependencyPath
+  ]
+
 -- | Options common to 'build', 'test', and 'browserify'
-buildTestBrowserifyArgs :: Array Args.Option
-buildTestBrowserifyArgs = [
+buildishArgs :: Array Args.Option
+buildishArgs = [
   Args.optionDefault "buildPath" ["--build-path", "-o"] Type.string
     "Path for compiler output." "./output",
-  Args.option "includePaths" ["--include", "-I"] Type.string
-    "Additional directories for PureScript source files, separated by spaces."
-  ]
+  Args.option "optimise" ["--optimise", "-O"] Type.flag
+    "Perform dead code elimination.",
+  Args.option "force" ["--force"] Type.flag
+    "Force a build even if no source files have changed."
+  ] ++ pathArgs
 
 buildArgs :: Array Args.Option
 buildArgs = [
   Args.optionDefault "main" ["--main", "-m"] Type.string
     "Application's entry point." "Main",
-  Args.option "optimise" ["--optimise", "-O"] Type.flag
-    "Perform dead code elimination.",
   Args.option "to" ["--to", "-t"] Type.string
-    "Output file name (stdout if not specified)."
-  ] ++ buildTestBrowserifyArgs
+    "Output file name (stdout if not specified).",
+  Args.option "modules" ["--modules"] Type.string
+    "Additional modules to be included in the output bundle (comma-separated list)."
+  ] ++ buildishArgs
 
 nop :: Args.Action
 nop _ = return unit
@@ -69,7 +102,7 @@ commands = [
       "Run test script using this command instead of Node.",
     Args.optionDefault "engine" ["--engine"] Type.string
       "Run the Application on a different JavaScript engine (node, iojs)" "node"
-    ] ++ buildTestBrowserifyArgs,
+    ] ++ buildishArgs,
   Args.command "browserify"
     "Produce a deployable bundle using Browserify." nop $ [
       Args.option "to" ["--to", "-t"] Type.string
@@ -86,22 +119,35 @@ commands = [
         "Don't run `pulp build` before browserifying.",
       Args.option "force" ["--force"] Type.flag
         "Force a non-incremental build by deleting the build cache."
-      ] ++ buildTestBrowserifyArgs,
+      ] ++ buildishArgs,
   Args.command "run" "Compile and run the project." nop $ [
     Args.optionDefault "engine" ["--engine"] Type.string
       "Run the Application on a different JavaScript engine (node, iojs)" "node"
     ] ++ buildArgs,
-  Args.command "docs" "Generate project documentation." nop [
+  Args.command "docs" "Generate project documentation." nop $ [
     Args.option "withTests" ["--with-tests", "-t"] Type.flag
       "Include tests.",
     Args.option "withDeps" ["--with-deps", "-d"] Type.flag
       "Include external dependencies."
-    ],
-  Args.command "psci"
-    "Launch a PureScript REPL configured for the project." nop []
+    ] ++ pathArgs,
+  Args.command "psci" "Launch a PureScript REPL configured for the project." nop pathArgs,
+  Args.command "server" "Launch a Webpack development server." nop $ [
+      Args.optionDefault "main" ["--main", "-m"] Type.string
+        "Application's entry point." "Main",
+      Args.option "config" ["--config", "-c"] Type.file
+        "Override the default Webpack config.",
+      Args.optionDefault "port" ["--port", "-p"] Type.int
+        "Port number to listen on." "1337",
+      Args.optionDefault "host" ["--host"] Type.string
+        "IP address to bind the server to." "localhost",
+      Args.option "noInfo" ["--no-info"] Type.flag
+        "Display no info to the console, only warnings and errors.",
+      Args.option "quiet" ["--quiet", "-q"] Type.flag
+        "Display nothing to the console when rebuilding."
+    ] ++ buildishArgs
   ]
 
-failed :: forall e. Error -> EffN (console :: CONSOLE | e) Unit
+failed :: forall e a. Error -> EffN (console :: CONSOLE | e) a
 failed err = do
   log $ "ERROR: " ++ show err
   exit 1

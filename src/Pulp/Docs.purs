f0fa747b0f7d2c22717eea2050e307e319747cd1
Harry Garrood <harry@garrood.me>
2015-11-28 16:26:08 +0100

    docs: Get module names from file contents
    There doesn't seem to be any other way to get around the fact that
    people tend to have module names not matching file names in their test/
    directory.

--- src/Pulp/Docs.purs
+++ src/Pulp/Docs.purs
@@ -2,16 +2,18 @@
 module Pulp.Docs where
 
 import Prelude
-import Control.Bind ((>=>), join)
 import Data.Tuple (Tuple(..))
 import Data.Maybe (Maybe(..))
 import qualified Data.Array as Array
 import qualified Data.Set as Set
 import qualified Data.Map as Map
-import Data.String.Regex (Regex(), regex, noFlags, match, replace)
-import Data.Foldable (mconcat, for_)
+import qualified Data.String as String
+import Data.Foldable (mconcat, for_, elem)
+import Data.Traversable (traverse)
 import Control.Monad
 import Control.Monad.Eff.Class (liftEff)
+import Node.FS.Aff as FS
+import Node.Encoding (Encoding(..))
 
 import Pulp.Args
 import Pulp.Args.Get
@@ -19,6 +21,7 @@ import Pulp.Exec
 import Pulp.Files
 import qualified Pulp.System.Process as Process
 import qualified Pulp.System.Log as Log
+import Pulp.System.FFI
 
 action :: Action
 action = Action \args -> do
@@ -28,43 +31,63 @@ action = Action \args -> do
   let opts = Map.union args.globalOpts args.commandOpts
 
   withTests <- getFlag "withTests" opts
-  globTest <- if withTests then testGlobs opts else pure Set.empty
-  globSrc <- Set.union globTest <$> defaultGlobs opts
-  globGen <- Set.union globTest <$> localGlobs opts
+  withDeps <- getFlag "withDeps" opts
+
+  let includeWhen b act = if b then act else pure Set.empty
+  optionalExtras <- Set.union <$> includeWhen withTests (testGlobs opts)
+                              <*> includeWhen withDeps  (dependencyGlobs opts)
+
+  globSrc <- Set.union optionalExtras <$> defaultGlobs opts
+  globGen <- Set.union optionalExtras <$> localGlobs opts
 
   genFiles <- resolveGlobs (sources globGen)
 
-  Tuple docgen fails <- pure $ mconcat $ flip map genFiles \path -> do
-    case matchModuleName path of
-      Just mn ->
-        Tuple ["--docgen", mn ++ ":" ++ docPathFor path] []
-      Nothing ->
-        Tuple [] [path]
+  Tuple docgen fails <- mconcat <$> traverse makeDocgen genFiles
 
   unless (Array.null fails) $ do
-    Log.log "Unable to generate documentation for the following modules:"
+    Log.err $ "Unable to extract module name from the following modules:"
     for_ fails (Log.log <<< ("  " ++))
-    Log.log "Please make sure your module names are consistent with your file paths."
+    Log.err $ "This may be a bug."
 
   _ <- execQuiet "psc-docs" (args.remainder ++ sources globSrc ++ docgen) Nothing
 
   Log.log "Documentation generated."
 
-moduleNameR :: Regex
-moduleNameR = regex "([A-Z][^\\/\\.]*(\\/|\\.))+" noFlags
+-- | Given a file path to be included in the documentation, return a --docgen
+-- | argument for it, to be passsed to psc-docs.
+makeDocgen :: forall e. String -> AffN e (Tuple (Array String) (Array String))
+makeDocgen path = do
+  maybeModName <- extractModuleName path
+  pure $ case maybeModName of
+    Just mn ->
+      Tuple ["--docgen", showModuleName mn ++ ":" ++ docPath mn] []
+    Nothing ->
+      Tuple [] [path]
 
-r :: String -> Regex
-r str = regex str noFlags
+-- | Given a module name, return the file path where its documentation should
+-- | be written to.
+docPath :: ModuleName -> String
+docPath mn = "docs/" ++ String.joinWith "/" mn ++ ".md"
 
-matchModuleName :: String -> Maybe String
-matchModuleName =
-  match moduleNameR
-    >=> Array.head
-    >>> join
-    >>> map (replace (r "\\.$") "" >>> replace pathSeparators ".")
+type ModuleName = Array String
+
+showModuleName :: ModuleName -> String
+showModuleName = String.joinWith "."
+
+-- | Given a PureScript source file path, extract its module name (or throw
+-- | an error).
+extractModuleName :: forall e. String -> AffN e (Maybe ModuleName)
+extractModuleName path = go <$> FS.readTextFile UTF8 path
   where
-  pathSeparators :: Regex
-  pathSeparators = regex "(\\/|\\\\)" (noFlags { global = true })
+  go = String.split "\n"
+        >>> map moduleNameFromLine
+        >>> Array.catMaybes
+        >>> Array.head
 
-docPathFor :: String -> String
-docPathFor = replace (r ".purs$") ".md" <<< replace (r "^(src|test)") "docs"
+-- | Given a line in a PureScript source file, attempt to extract its name.
+moduleNameFromLine :: String -> Maybe ModuleName
+moduleNameFromLine =
+  String.stripPrefix "module "
+  >>> map (   String.takeWhile (not <<< (`elem` [' ', '(']))
+          >>> String.split "."
+          )

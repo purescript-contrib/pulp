3d393be3f2839e493d1b4465ef4d34142610ed36
Harry Garrood <harry@garrood.me>
2015-11-30 13:41:42 +0000

    Remove use of 'exit' in validate

--- src/Pulp/Validate.purs
+++ src/Pulp/Validate.purs
@@ -8,10 +8,11 @@ import Data.String (trim)
 import Data.Version.Haskell (Version(..), parseVersion, showVersion)
 import Control.Monad (when)
 import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Exception (error)
+import Control.Monad.Error.Class (throwError)
 import Text.Parsing.Parser (ParseError(..))
 
 import Pulp.Exec (execQuiet)
-import Pulp.System.Process (exit)
 import Pulp.System.FFI
 import Pulp.System.Log as Log
 
@@ -23,7 +24,7 @@ validate = do
               <> showVersion minimumPscVersion <> " or higher."
     Log.err $ "Your installed version is " <> showVersion ver <> "."
     Log.err $ "Please either upgrade PureScript or downgrade Pulp to version 3.x."
-    liftEff $ exit 1
+    throwError $ error "Minimum psc version not satisfied"
 
 getPscVersion :: forall e. AffN e Version
 getPscVersion = do
@@ -34,7 +35,7 @@ getPscVersion = do
     Left (ParseError { message: msg }) -> do
       Log.err $ "Unable to parse the version from psc. (It was: " <> verStr <> ")"
       Log.err $ "Please check that the right psc is on your PATH."
-      liftEff $ exit 1
+      throwError $ error "Couldn't parse version from psc"
 
 minimumPscVersion :: Version
 minimumPscVersion = Version (toList [0, 7, 0, 0]) Nil

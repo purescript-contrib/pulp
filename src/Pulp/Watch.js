65ca7ea0c3dccb0b52d17463f871887cf5f33c4d
Harry Garrood <harry@garrood.me>
2015-11-30 13:00:46 +0000

    Fix FFI declaration for watch

--- src/Pulp/Watch.js
+++ src/Pulp/Watch.js
@@ -4,12 +4,14 @@
 
 exports.watch = function watch(directories) {
   return function(act) {
-    var Watchpack = require("watchpack");
-    var watchpack = new Watchpack();
-    watchpack.watch([], directories, Date.now() - 10000);
-    watchpack.on("change", function(path) {
-      act(path)();
-    });
+    return function() {
+      var Watchpack = require("watchpack");
+      var watchpack = new Watchpack();
+      watchpack.watch([], directories, Date.now() - 10000);
+      watchpack.on("change", function(path) {
+        act(path)();
+      });
+    };
   };
 };
 

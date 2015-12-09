2beb4dce12d32a3195f5d560ece1f097a1770968
Harry Garrood <harry@garrood.me>
2015-12-09 08:47:52 +0100

    Fix initial PureScript files in Pulp.Init
    They accidentally got included in a find-and-replace to remove type
    variables.

--- src/Pulp/Init.purs
+++ src/Pulp/Init.purs
@@ -43,7 +43,7 @@ mainFile = unlines [
   "import Control.Monad.Eff",
   "import Control.Monad.Eff.Console",
   "",
-  "main :: Eff (console :: CONSOLE | e) Unit",
+  "main :: forall e. Eff (console :: CONSOLE | e) Unit",
   "main = do",
   "  log \"Hello sailor!\""
   ]
@@ -56,7 +56,7 @@ testFile = unlines [
   "import Control.Monad.Eff",
   "import Control.Monad.Eff.Console",
   "",
-  "main :: Eff (console :: CONSOLE | e) Unit",
+  "main :: forall e. Eff (console :: CONSOLE | e) Unit",
   "main = do",
   "  log \"You should add some tests.\""
   ]

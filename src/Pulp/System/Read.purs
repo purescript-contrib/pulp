e9e1da687527dd1548121e60cafc6e9689163e97
Harry Garrood <harry@garrood.me>
2016-02-28 12:50:01 +0000

    Add `pulp login`, part of #57

---
+++ src/Pulp/System/Read.purs
@@ -0,0 +1,14 @@
+module Pulp.System.Read ( read ) where
+
+import Prelude
+
+import Data.Function (Fn2(), runFn2)
+
+import Pulp.System.FFI (AffN(), Callback(), runNode)
+
+type ReadOptions = { prompt :: String, silent :: Boolean }
+
+foreign import read' :: Fn2 ReadOptions (Callback String) Unit
+
+read :: ReadOptions -> AffN String
+read opts = runNode $ runFn2 read' opts

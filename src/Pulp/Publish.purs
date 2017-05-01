a9c1ae915d8558d850af1a9db0eb2bdc02aea008
Thor Adam <thor@thoradam.com>
2017-05-01 21:05:29 +0000

    Check for bower.json in publish #243

--- src/Pulp/Publish.purs
+++ src/Pulp/Publish.purs
@@ -45,6 +45,8 @@ import Pulp.Login (tokenFilePath)
 
 action :: Action
 action = Action \args -> do
+  checkBowerProject
+
   out <- getOutputter args
 
   requireCleanGitWorkingTree
@@ -83,6 +85,13 @@ action = Action \args -> do
               <> " in this repository. This should not have happened. Please"
               <> " report this: https://github.com/bodil/pulp/issues/new")))
           pure
+
+checkBowerProject :: AffN Unit
+checkBowerProject = do
+  bower <- FS.exists "bower.json"
+  if bower then pure unit
+    else throwError
+         (error "No bower.json found, publishing is currently only implemented for Bower projects.")
 
 gzip :: String -> AffN Buffer
 gzip str = do
